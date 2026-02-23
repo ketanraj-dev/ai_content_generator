e31b87dd4a1bcc310a44dd421dddd7bbf08799e9
ketanraj-dev <ketanraj612@gmail.com>
2026-02-23 20:06:20 +0530

    Made Linkedin Connection

--- backend/app/api/linkedin_auth.py
+++ backend/app/api/linkedin_auth.py
@@ -6,23 +6,28 @@ from app.db.user_models import User
 from app.core.config import settings
 import requests
 from datetime import datetime, timedelta
+from app.db.database import get_db
+from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/auth/linkedin", tags=["linkedin"])
 
 @router.get("/login")
 def linkedin_login(current_user: User = Depends(get_current_user)):
+
     linkedin_url = (
         "https://www.linkedin.com/oauth/v2/authorization"
         f"?response_type=code"
         f"&client_id={settings.LINKEDIN_CLIENT_ID}"
         f"&redirect_uri={settings.LINKEDIN_REDIRECT_URI}"
         f"&scope=w_member_social openid profile email"
+        f"&state={current_user.id}"
     )
 
     return RedirectResponse(linkedin_url)
 
 @router.get("/callback")
-def linkedin_callback(code: str, current_user: User = Depends(get_current_user)):
+def linkedin_callback(code: str, state: str, db: Session = Depends(get_db)):
+
     token_url = "https://www.linkedin.com/oauth/v2/accessToken"
 
     data = {
@@ -43,15 +48,14 @@ def linkedin_callback(code: str, current_user: User = Depends(get_current_user))
     access_token = token_data["access_token"]
     expires_in = token_data["expires_in"]
 
-    db = SessionLocal()
-    user = db.query(User).filter(User.id == current_user.id).first()
+    user = db.query(User).filter(User.id == int(state)).first()
+
+    if not user:
+        raise HTTPException(status_code=400, detail="Invalid user")
 
     user.linkedin_access_token = access_token
     user.linkedin_expires_at = datetime.utcnow() + timedelta(seconds=expires_in)
 
     db.commit()
-    db.close()
 
     return {"message": "LinkedIn connected successfully"}
-
-
--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from app.core.config import settings
+from typing import Generator
 
-engine = create_engine(settings.DATABASE_URL,pool_pre_ping=True)
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -11,3 +12,11 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
